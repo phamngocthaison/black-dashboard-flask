c77cfd04f48046570b2fc88aff8534bc63795d3c
Son Pham <sonpham@AP-0523-1015.local>
2024-12-17 09:46:15 +0700

    add order detail in create_order

--- apps/sale/routes.py
+++ apps/sale/routes.py
@@ -2,7 +2,8 @@
 
 from apps.sale import blueprint
 from flask import render_template, request, redirect, url_for
-from apps.sale.models import SaleOrder
+from apps.sale.models import SaleOrder, OrderDetails
+from apps.product.models import Products
 from apps.customer.models import Customer
 from apps import db
 from datetime import datetime
@@ -29,6 +30,22 @@ def orders():
 #     return render_template('sale/create_order.html')
 #
 
+# @blueprint.route('/create_order', methods=['GET', 'POST'])
+# def create_order():
+#     if request.method == 'POST':
+#         customer_id = request.form['customer_id']
+#         order_date = datetime.strptime(request.form['order_date'], '%Y-%m-%d').date()
+#         total_amount = request.form['total_amount']
+#
+#         new_order = SaleOrder(customer_id=customer_id, order_date=order_date, total_amount=total_amount)
+#         db.session.add(new_order)
+#         db.session.commit()
+#
+#         return redirect(url_for('sale_blueprint.orders'))
+#
+#     customers = Customer.query.all()
+#     return render_template('sale/create_order.html', customers=customers)
+
 @blueprint.route('/create_order', methods=['GET', 'POST'])
 def create_order():
     if request.method == 'POST':
@@ -40,7 +57,24 @@ def create_order():
         db.session.add(new_order)
         db.session.commit()
 
+        # Handle order details
+        product_ids = request.form.getlist('product_id')
+        quantities = request.form.getlist('quantity')
+        unit_prices = request.form.getlist('unit_price')
+
+        for product_id, quantity, unit_price in zip(product_ids, quantities, unit_prices):
+            order_detail = OrderDetails(
+                order_id=new_order.id,
+                product_id=product_id,
+                quantity=quantity,
+                unit_price=unit_price
+            )
+            db.session.add(order_detail)
+
+        db.session.commit()
+
         return redirect(url_for('sale_blueprint.orders'))
 
     customers = Customer.query.all()
-    return render_template('sale/create_order.html', customers=customers)
+    products = Products.query.all()
+    return render_template('sale/create_order.html', customers=customers, products=products)
